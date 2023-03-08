55fbdd61c6d87ae910f4e86e282bab1e15cab6eb
Nayan Nakum <NayanNakum@rightpoint.com>
2023-03-08 19:51:10 +0530

    lombok working

--- src/main/java/com/demo/springmongo/model/Author.java
+++ src/main/java/com/demo/springmongo/model/Author.java
@@ -1,33 +1,13 @@
 package com.demo.springmongo.model;
 
+import lombok.Data;
+
+@Data
+
 public class Author {
 
 	private int id;
 	private String name;
 	private int numberOfBooks;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getNumberOfBooks() {
-		return numberOfBooks;
-	}
-
-	public void setNumberOfBooks(int numberOfBooks) {
-		this.numberOfBooks = numberOfBooks;
-	}
 
 }
--- src/main/java/com/demo/springmongo/model/Book.java
+++ src/main/java/com/demo/springmongo/model/Book.java
@@ -3,60 +3,19 @@ package com.demo.springmongo.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Document(collection = "book")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Book {
 	@Id
 	private int id;
 	private String bookName;
 	private double price;
 	private Author author;
-
-	public Book(int id, String bookName, double  price, Author author) {
-		super();
-		this.id = id;
-		this.bookName = bookName;
-		this.price = price;
-		this.author = author;
-	}
-
-	public Book() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public Author getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(Author author) {
-		this.author = author;
-	}
-
-
-
-	public double getPrice() {
-		return price;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getBookName() {
-		return bookName;
-	}
-
-	public void setBookName(String bookName) {
-		this.bookName = bookName;
-	}
-
 
 }
